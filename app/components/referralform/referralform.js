779727ed7449be638f77f5d5dc7767f324acedf3
Rob Retchless <retchles@ca.ibm.com>
2015-09-17 21:27:16 -0400

    add some styles

--- app/components/referralform/referralform.js
+++ app/components/referralform/referralform.js
@@ -7,10 +7,43 @@ export default class ReferralForm extends React.Component {
       <div className='app'>
      	  <h3>Refer to another Wedding Photographer</h3>
         <form>
-
+          <table>
+            <tr>
+             <td>Referring photographer</td>
+             <td><input type="text"/></td>
+            </tr>
+            <tr>
+              <td>Client first name:</td>
+              <td><input type="text"/></td>
+            </tr>
+            <tr>
+              <td>Client last name:</td>
+              <td><input type="text"/></td>
+            </tr>
+            <tr>
+              <td>Wedding date (including year):</td>
+              <td><input type="text"/></td>
+            </tr>
+            <tr>
+              <td>Wedding city</td>
+              <td><input type="text"/></td>
+            </tr>
+            <tr>
+              <td>Wedding venue</td>
+              <td><input type="text"/></td>
+            </tr>
+            <tr>
+              <td>Additional notes</td>
+              <td><textarea name="message" rows="10" cols="30"></textarea></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><button>Submit</button></td>
+            </tr>
+          </table>
         </form>
       </div>
     );
   }
-  
+
 }
